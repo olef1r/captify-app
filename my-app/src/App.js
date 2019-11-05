ccec70253b99aa4081d063e868a8273e881ca4d6
Anastasiia <aolef17@gmail.com>
2019-11-05 18:06:37 +0200

    add btn

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import Header from './components/layouts/Header';
 import Footer from './components/layouts/Footer';
+import SimpleTable from './components/Table';
+import Button from './components/Button';
+import { func } from 'prop-types';
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+function handleClick() {
+  console.log("!!!")
+}
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SimpleTable rows={rows}/>
+      <Button onClick={handleClick} / >
       <Footer />
     </div>
   );
---
+++ my-app/src/components/Table.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 650,
+  },
+});
+
+
+
+export default function SimpleTable({rows}) {
+  const classes = useStyles();
+  
+  //const { rows } = this.props
+  return (
+    <Paper className={classes.root}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
+  );
+}
